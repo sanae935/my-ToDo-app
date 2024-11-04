bed316e70ac5ab8eda5f88fabaa64785049b2c61
sanae935 <s.elatmani005@gmail.com>
2024-11-03 20:52:24 -0500

    pushing file

--- app.js
+++ app.js
@@ -2,3 +2,28 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3000;
+let tasks = [];
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+// Routes
+app.get('/', (req, res) => {
+    console.log("Rendering index view");
+    res.render('index', { tasks });
+});
+
+app.post('/add-task', (req, res) => {
+    const newTask = req.body.task;
+    if (newTask) {
+        tasks.push(newTask);
+    }
+    res.redirect('/');
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
